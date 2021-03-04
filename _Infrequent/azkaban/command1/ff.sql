6da0c851cb03f4fe27fa04558d59db3887e089cd
xuriyongjiu <xuriyongjiu@sina.com>
2021-03-04 22:11:12 +0800

    haha

--- _Infrequent/azkaban/command1/ff.sql
+++
@@ -1,2 +0,0 @@
-drop table if exists ff;
-create table ff as select * from tj t1 left join tt t2 on t1.did=t2.id;
--- _Infrequent/azkaban/command1/tt.sql
+++
@@ -1,1 +0,0 @@
-load data local inpath '/home/tyx/data/tt.txt' overwrite into table dbazkaban.tt;
